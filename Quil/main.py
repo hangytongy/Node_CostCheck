38cf8f983f2ebb21574d4c5dce3f6a7ee5f33797
hangytongy <145353377+hangytongy@users.noreply.github.com>
2024-08-26 17:54:04 +0800

    Update main.py

--- Quil/main.py
+++ Quil/main.py
@@ -1,10 +1,13 @@
 import subprocess
 import os
+from pathlib import Path
+import pandas as pd
+from datetime import datetime
 
 def run_ansible_playbook(playbook_path, inventory_path):
-    
+
     os.environ['ANSIBLE_HOST_KEY_CHECKING'] = 'False'
-    
+
     try:
         result = subprocess.run(
             ['ansible-playbook', '-i', inventory_path, playbook_path],
@@ -19,9 +22,45 @@ def run_ansible_playbook(playbook_path, inventory_path):
         print("An error occurred while executing the playbook.")
         print("STDOUT:", e.stdout)
         print("STDERR:", e.stderr)
-    retrun result.stdout
+    return result.stdout
 
 # Example usage
 playbook_path = 'quil_query.yml'
 inventory_path = 'hosts'
 result = run_ansible_playbook(playbook_path, inventory_path)
+print("------------")
+
+directory = os.getcwd()
+file_path = os.path.join(directory,'ansi_quil.txt')
+
+parent_directory = Path(file_path).parent
+parent_directory.mkdir(parents=True, exist_ok=True)
+
+with open(file_path, 'w') as file:
+    file.write(result)
+
+version = []
+balance = []
+
+with open(file_path, 'r') as file:
+    for line in file:
+        if "Version:" in line:
+            print(line.strip())
+            val = line.strip().split(":")[1].strip().split("'")[1]
+            print(val)
+            version.append(str(val))
+        if "Unclaimed Balance:" in line:
+            print(line.strip())
+            bal = line.strip().split(":")[1].strip().split("'")[1].split()[0]
+            print(bal)
+            balance.append(float(bal))
+
+data = { 'version' : version, 'balance' : balance }
+data['date'] = datetime.now().strftime("%Y-%m-%d")
+df = pd.DataFrame(data)
+
+print(df)
+
+csv_path = os.path.join(directory,'data_quil.csv')
+file_exists = os.path.isfile(csv_path)
+df.to_csv(csv_path, index=False, mode = 'a', header = not file_exists)
