c1f772ceacb446145b7bae4cedaace81858c6ee2
hangytongy <145353377+hangytongy@users.noreply.github.com>
2024-08-27 13:48:03 +0800

    Update plotting.py

--- Quil/plotting.py
+++ Quil/plotting.py
@@ -13,26 +13,31 @@ def plot_df():
 
   df = pd.read_csv(file_path)
 
-  df['balance'] = df['balance'].apply(lambda x : float(x))
-  df['time'] = pd.to_datetime(df['date'])
+  if len(df) <=1 :
+    return False
 
-  #get server price
-  server_price = 10/(30*24*60)
+  else:
 
-  df['quil_earned'] = df['balance'].diff()
-  df['quil_profit'] = df['quil_earned']*df['quil_price']
-  df['time_delta'] = df['time'].diff().dt.total_seconds() / 60
-  df['server_cost'] = server_price* df['time_delta']
-
-  plt.figure(figsize=(12,6))
-  plt.plot(df['time'], df['quil_earned'], linestyle='-', marker='s',color='b', label = 'quil earned')  # Line plot
-  plt.plot(df['time'], df['quil_profit'], color = 'r', marker='o', label = 'quil profit')
-  plt.plot(df['time'], df['server_cost'], color = 'g', label = 'server cost')
-  plt.plot(df['time'], df['quil_price'], color = 'orange', label = 'quil price')
-  plt.title('Quil Mining Profitability')
-  plt.xlabel('Time')
-  plt.ylabel('Balance')
-  plt.legend(loc='best')
-  plt.savefig(f'{charts_path}_Quil_Balance.png')
-
-  return f"{charts_path}_Quil_Balance.png"
+    df['balance'] = df['balance'].apply(lambda x : float(x))
+    df['time'] = pd.to_datetime(df['date'])
+  
+    #get server price
+    server_price = 10/(30*24*60)
+  
+    df['quil_earned'] = df['balance'].diff()
+    df['quil_profit'] = df['quil_earned']*df['quil_price']
+    df['time_delta'] = df['time'].diff().dt.total_seconds() / 60
+    df['server_cost'] = server_price* df['time_delta']
+  
+    plt.figure(figsize=(12,6))
+    plt.plot(df['time'], df['quil_earned'], linestyle='-', marker='s',color='b', label = 'quil earned')  # Line plot
+    plt.plot(df['time'], df['quil_profit'], color = 'r', marker='o', label = 'quil profit')
+    plt.plot(df['time'], df['server_cost'], color = 'g', label = 'server cost')
+    plt.plot(df['time'], df['quil_price'], color = 'orange', label = 'quil price')
+    plt.title('Quil Mining Profitability')
+    plt.xlabel('Time')
+    plt.ylabel('Balance')
+    plt.legend(loc='best')
+    plt.savefig(f'{charts_path}_Quil_Balance.png')
+  
+    return f"{charts_path}_Quil_Balance.png"
